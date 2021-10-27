8a152dc3a4d5c2587c6cb42555b894ba2012b928
p22cobrien <p22cobrien@fairifeldprep.org>
2021-10-27 18:01:46 -0400

    add lab_5-1.py

---
+++ lab_5-1.py
@@ -0,0 +1,11 @@
+# Author: CMOB 10/27/2021
+
+school = "Fairfield Prep"
+
+first_half = school[0:9]
+second_half = school[9:14]
+
+print(first_half)
+print(second_half)
+
+print(first_half + second_half)
