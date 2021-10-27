f58ea60061132cc922043b7a8e484059384fada8
p22cobrien <p22cobrien@fairifeldprep.org>
2021-10-27 17:53:15 -0400

    add lab_4-1.py

---
+++ lab_4-1.py
@@ -0,0 +1,13 @@
+# Author: CMOB 10/27/2021
+
+print("Fairfield " + "Prep")
+
+print('-' * 20)
+
+print("I'm loving this short story I'm reading, \"The Fall of the House of usher\"")
+
+print(len("I'm loving this short story I'm reading, \"The Fall of the House of usher\""))
+
+apple = "apple"
+letter_l = apple[3]
+print(letter_l)
