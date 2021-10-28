9cd48e0be9d98c5e26b336c0e2dfd14fdc4927ce
p22cobrien <p22cobrien@fairifeldprep.org>
2021-10-27 21:07:19 -0400

    submit lab_5-2.py

---
+++ lab_5-2.py
@@ -0,0 +1,10 @@
+# Author: CMOB 10/27/2021
+
+word = "arcane"
+
+if word < "banana":
+    print( word + " is smaller than banana.")
+elif word > "banana":
+    print(word + " is larger than banana.")
+else:
+    print("Banana for scale")
