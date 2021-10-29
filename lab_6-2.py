628404377d14aa9f72444b6299f00bc6b70a6b4c
p22cobrien <p22cobrien@fairfieldprep.org>
2021-10-29 10:51:05 -0400

    add lab-6-2.py

---
+++ lab_6-2.py
@@ -0,0 +1,6 @@
+# Author: CMOB 10/29/2021
+
+person1 = input("What is your name? ")
+person2 = input("What is the other person's name? ")
+
+print("Hello, {0}. My name is {1}.".format(person1, person2))
